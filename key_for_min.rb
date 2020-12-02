c355f1a5ebd3b96eaa6b8af31be688e4c4e7a10b
darien bryant <darienbryant11@gmail.com>
2020-12-02 18:26:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash,key)
 
   lowest = nil  
-name_hash.each do |name_hash |
+name_hash.each do |key, value  |
   if name_hash < lowest 
     lowest << name_hash 
   end 
